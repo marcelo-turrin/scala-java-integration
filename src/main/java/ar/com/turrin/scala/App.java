62defb4447e27af9cd1612983a64a94c616ec268
Marcelo Turrin <marcelo.turrin@gmail.com>
2014-11-09 12:01:20 -0300

    Created a simple java project with maven archetype generate mvn archetype:generate -DgroupId=ar.com.turrin.scala 			-DartifactId=scala-java-integration 			-DarchetypeArtifactId=maven-archetype-quickstart 			-DinteractiveMode=false

---
+++ src/main/java/ar/com/turrin/scala/App.java
@@ -0,0 +1,13 @@
+package ar.com.turrin.scala;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
