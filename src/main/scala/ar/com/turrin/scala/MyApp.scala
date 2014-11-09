ee2bc0b8682407bfbfe8fddfa875bb45fb1b0374
Marcelo Turrin <marcelo.turrin@gmail.com>
2014-11-09 14:37:29 -0300

    eclipse configuration for scalawq

---
+++ src/main/scala/ar/com/turrin/scala/MyApp.scala
@@ -0,0 +1,7 @@
+package ar.com.turrin.scala
+
+object MyApp {
+
+  def main(args: Array[String]): Unit = println("hola mundo")
+
+}
---
+++ src/test/scala/ar/com/turrin/scala/MyAppTest.scala
@@ -0,0 +1,5 @@
+package ar.com.turrin.scala
+
+class MyAppTest {
+
+}
